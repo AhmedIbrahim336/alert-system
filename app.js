194bb8a87aeee0ff8a7cd71c108de5a253184b65
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-03 08:37:57 +0200

    show the alert form the left

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const btnLeft = document.getElementById('left');
+const alert = document.getElementById('alert');
 
 btnLeft.addEventListener('click', e => {
-  alert('click!');
+  alert.classList.remove('hide-alert');
+  alert.classList.add('alert-left');
 });
