8c3d92a0173d91fe6378c6904c9e365e753edb22
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-05 20:02:05 +0200

    remove unused js

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const action = document.getElementById('action');
 
-btnLeft.addEventListener('click', e => {
-  //   alert.classList.remove('hide-alert');
-  //   alert.classList.add('show-alert-left');
-  alert.classList = alert.classList.filter(c => c !== 'show-alert-left');
-});
-
-butRight.addEventListener('click', e => {
+action.addEventListener('click', e => {
   alert.classList.toggle('hide-alert');
   alert.classList.add('show-alert-right');
 });
