6a7b20d5ef47a8687fbb32581b368fb0298e0fc0
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-05 20:04:32 +0200

    Remove the alert from the DOM when click the undo button

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ action.addEventListener('click', e => {
   alert.classList.toggle('hide-alert');
   alert.classList.add('show-alert-right');
 });
+
+undo.addEventListener('click', e => {
+  alert.classList.add('hide-alert');
+  alert.classList.remove('show-alert-right');
+});
