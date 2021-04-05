955da4fe695f5751a9d290e3f43ef4f5595737bc
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-05 20:03:20 +0200

    get the undo button form the DOM

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const action = document.getElementById('action');
 const alert = document.getElementById('alert');
+const undo = document.getElementById('undo');
 
 action.addEventListener('click', e => {
   alert.classList.toggle('hide-alert');
