f7f3e74ab163208dff587c3a006930ef699cd8a3
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-05 19:59:03 +0200

    Update the UI

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const btnLeft = document.getElementById('left');
+const butRight = document.getElementById('right');
 const alert = document.getElementById('alert');
 
 btnLeft.addEventListener('click', e => {
-  alert.classList.remove('hide-alert');
-  alert.classList.add('alert-left');
+  //   alert.classList.remove('hide-alert');
+  //   alert.classList.add('show-alert-left');
+  alert.classList = alert.classList.filter(c => c !== 'show-alert-left');
+});
+
+butRight.addEventListener('click', e => {
+  alert.classList.toggle('hide-alert');
+  alert.classList.add('show-alert-right');
 });
